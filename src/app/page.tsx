6a58fa6b792cd7caee6115a1816036c0d6b8c140
Lxg1c <teran_2005@mail.ru>
2025-05-14 18:12:12 +0300

    initialized start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,61 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import Link from "next/link";
+import Input from "@/shared/ui/input/Input";
+import "./home.css"
+import Image from "next/dist/client/legacy/image";
+import userIcon from "../../public/user.svg"
+import Checkbox from "@/shared/ui/checkbox/Checkbox";
+import {useState} from "react";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+export default function Page() {
+    const [active, setActive] = useState(false);
+
+    return (
+        <section className="home h-screen mt-26 relative overflow-hidden">
+            <div className="home__container container relative z-10">
+                <div className="home__container-top max-w-180">
+                    <h1 className="title-1 text-center">Выбери свое лучшее место для жизни</h1>
+                    <Link href="/" className="text-center block w-full title-7 mt-4">
+                        Где бы вы хотели жить?
+                    </Link>
+                </div>
+
+                <div className="flex items-center max-w-150 mt-10 gap-4">
+                    <Input placeholder="Введите имя пользователя"/>
+                    <button className='search-btn bg-green-500 text-white cursor-pointer rounded-xl'>
+                        Мои отчеты
+                    </button>
+                </div>
+
+                <form
+                    className="mt-10 form p-10 bg-white/30 backdrop-blur-sm rounded-xl shadow-md flex items-center justify-between">
+                    <div className='user__info'>
+                        <div className='user__profile flex items-end gap-5'>
+                            <Image src={userIcon} width='80' height='80' alt="User icon"/>
+                            <div className='user__name'>
+                                <h2 className='title-2'>Никита</h2>
+                                <p className='title-7 mt-1.5'>Имя пользователя</p>
+                            </div>
+                        </div>
+
+                        <div className="user__phone mt-5">
+                            <h2 className='title-5'>+79281484862</h2>
+                            <p className='title-7 mt-1.5'>Номер телефона</p>
+                        </div>
+
+                        <h2 className='title-2 mt-10 mb-10'>Предпочтения</h2>
+                        <ul className='max-w-75 mt-4 flex flex-col gap-4'>
+                            <li className='flex justify-between items-center'>
+                                <p className='title-4'>Больница</p>
+                                <Checkbox checked={active} onChange={() => setActive(!active)}/>
+                            </li>
+                        </ul>
+                    </div>
+
+                </form>
+            </div>
+        </section>
+    )
 }
