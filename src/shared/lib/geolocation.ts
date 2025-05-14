0f6d310f73115251842b0d020d1ca66fb6004b65
Lxg1c <teran_2005@mail.ru>
2025-05-14 18:33:02 +0300

    create func to get user location

---
+++ src/shared/lib/geolocation.ts
@@ -0,0 +1,18 @@
+export const getUserLocation = (): Promise<GeolocationPosition> => {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject(new Error('Geolocation is not supported by your browser'));
+            return;
+        }
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => resolve(position),
+            (error) => reject(error),
+            {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            }
+        );
+    });
+};
